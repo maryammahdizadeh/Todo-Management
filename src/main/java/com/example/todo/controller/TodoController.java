75358d90ca6819c8c0e7c6609d22f8705266ee41
maryam <maryam19.mahdizadeh@gmail.com>
2025-05-13 19:32:04 +0330

    Make add todo method as rest API in TodoController

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -5,6 +5,7 @@ import com.example.todo.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class TodoController {
 
     // Build Add Todo REST API
 
+    @PostMapping
     public ResponseEntity<TodoDto> addTodo(TodoDto todoDto) {
 
         TodoDto savedTodoDto = todoService.addTodo(todoDto);
