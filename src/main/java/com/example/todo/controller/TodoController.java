e77530cfa8b2ecb3f85ac5e198d262a6133a67f0
maryam <maryam19.mahdizadeh@gmail.com>
2025-05-13 19:30:32 +0330

    Create add todo method in TodoController

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -1,7 +1,10 @@
 package com.example.todo.controller;
 
+import com.example.todo.dto.TodoDto;
 import com.example.todo.service.TodoService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoController {
 
     private TodoService todoService;
+
+    // Build Add Todo REST API
+
+    public ResponseEntity<TodoDto> addTodo(TodoDto todoDto) {
+
+        TodoDto savedTodoDto = todoService.addTodo(todoDto);
+
+        return new ResponseEntity<>(savedTodoDto, HttpStatus.CREATED);
+    }
 }
