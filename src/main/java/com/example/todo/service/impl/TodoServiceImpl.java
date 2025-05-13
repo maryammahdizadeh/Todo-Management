7d18144b49512aed20a56008ad5f1784d871cd56
maryam <maryam19.mahdizadeh@gmail.com>
2025-05-13 18:40:48 +0330

    Provide the implementation and override addTodo method

--- src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
@@ -1,4 +1,13 @@
 package com.example.todo.service.impl;
 
-public class TodoServiceImpl {
+import com.example.todo.dto.TodoDto;
+import com.example.todo.service.TodoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoServiceImpl implements TodoService {
+    @Override
+    public TodoDto addTodo(TodoDto todoDto) {
+        return null;
+    }
 }
