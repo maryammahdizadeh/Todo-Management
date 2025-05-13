8750cfbf17bc819b8aefd02bcf2735dd129ee730
maryam <maryam19.mahdizadeh@gmail.com>
2025-05-13 18:45:12 +0330

    Inject the required dependencies

--- src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
@@ -1,11 +1,17 @@
 package com.example.todo.service.impl;
 
 import com.example.todo.dto.TodoDto;
+import com.example.todo.repository.TodoRepository;
 import com.example.todo.service.TodoService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class TodoServiceImpl implements TodoService {
+
+    private TodoRepository todoRepository;
+
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
         return null;
