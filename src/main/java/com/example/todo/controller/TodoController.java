97d5f31ed88af2cd26af0a785859561e56916514
maryam <maryam19.mahdizadeh@gmail.com>
2025-05-13 19:22:23 +0330

    Inject TodoService dependency in TodoController

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -1,9 +1,14 @@
 package com.example.todo.controller;
 
+import com.example.todo.service.TodoService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/todos")
+@AllArgsConstructor
 public class TodoController {
+
+    private TodoService todoService;
 }
