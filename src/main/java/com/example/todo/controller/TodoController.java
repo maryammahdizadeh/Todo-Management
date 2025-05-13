3bf1644b017bc39f32c5037cca37e57434fa7d21
maryam <maryam19.mahdizadeh@gmail.com>
2025-05-13 19:18:45 +0330

    Define base URl for all rest APIs in TodoController

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -1,7 +1,9 @@
 package com.example.todo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/todos")
 public class TodoController {
 }
