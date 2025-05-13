23768736290e6b5b5bb78732046b61c124f33b14
maryam <maryam19.mahdizadeh@gmail.com>
2025-05-13 18:50:06 +0330

    Convert TodoDto into Todo entity for implement addTodo method

--- src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.todo.service.impl;
 
 import com.example.todo.dto.TodoDto;
+import com.example.todo.entity.Todo;
 import com.example.todo.repository.TodoRepository;
 import com.example.todo.service.TodoService;
 import lombok.AllArgsConstructor;
@@ -14,6 +15,12 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
+
+        // convert TodoDto into Todo Jpa entity
+        Todo todo = new Todo();
+        todo.setTitle(todoDto.getTitle());
+        todo.setDescription(todoDto.getDescription());
+        todo.setCompleted(todoDto.isCompleted());
         return null;
     }
 }
