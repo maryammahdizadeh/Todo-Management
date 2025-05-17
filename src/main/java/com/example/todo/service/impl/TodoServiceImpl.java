3283152882ff595d2efdac5433d8cabc7d08ba77
maryam <maryam19.mahdizadeh@gmail.com>
2025-05-17 15:21:37 +0330

    Remove the commented code

--- src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
@@ -20,25 +20,13 @@ public class TodoServiceImpl implements TodoService {
     public TodoDto addTodo(TodoDto todoDto) {
 
         // convert TodoDto into Todo Jpa entity
-//        Todo todo = new Todo();
-//        todo.setTitle(todoDto.getTitle());
-//        todo.setDescription(todoDto.getDescription());
-//        todo.setCompleted(todoDto.isCompleted());
-
         Todo todo = modelMapper.map(todoDto, Todo.class);
 
         // save Todo Jpa entity into database
         Todo savedTodo = todoRepository.save(todo);
 
         // Convert saved Todo Jpa entity object into TodoDto object
-//        TodoDto savedTodoDto = new TodoDto();
-//        savedTodoDto.setId(savedTodo.getId());
-//        savedTodoDto.setTitle(savedTodo.getTitle());
-//        savedTodoDto.setDescription(savedTodo.getDescription());
-//        savedTodoDto.setCompleted(savedTodo.isCompleted());
 
-        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
-
-        return savedTodoDto;
+        return modelMapper.map(savedTodo, TodoDto.class);
     }
 }
