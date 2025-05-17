6a624f879546b70bf7bb84eb21aae6729194a73a
maryam <maryam19.mahdizadeh@gmail.com>
2025-05-17 16:11:47 +0330

    Create constructor for ResourceNotFoundException

--- src/main/java/com/example/todo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/todo/exception/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.todo.exception;
 
+
 public class ResourceNotFoundException extends RuntimeException {
 
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
